3792c840f5a949e8003acc45cf687ab7b16f5f05
anazks <78355363+anazks@users.noreply.github.com>
2021-09-22 08:53:49 -0700

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-defined('server') ? null : define("server", "localhost");
-defined('user') ? null : define ("user", "root") ;
-defined('pass') ? null : define("pass","");
-defined('database_name') ? null : define("database_name", "erisdb") ;
+defined('server') ? null : define("server", "sql5.freesqldatabase.com");
+defined('user') ? null : define ("user", "sql5439255") ;
+defined('pass') ? null : define("pass","R4LPLdWd1Z");
+defined('database_name') ? null : define("database_name", "sql5439255") ;
 
 $this_file = str_replace('\\', '/', __File__) ;
 $doc_root = $_SERVER['DOCUMENT_ROOT'];
